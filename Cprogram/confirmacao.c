d3ae159b3b231e5e0200f06bbf5d14d6d700b26d
MATHEUS FERREIRA RIBEIRO DA SILVA <matheus.silva475@cs.cruzeirodosul.edu.br>
2026-09-01 21:36:54 -0300

    adicionei C e php

--- Cprogram/confirmacao.c
+++ Cprogram/confirmacao.c
@@ -2,3 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        printf("Uso: %s <codigo> <valor> <metodo>\n", argv[0]);
+        return 1;
+    }
+
+    char *codigo = argv[1];
+    char *valor = argv[2];
+    char *metodo = argv[3];
+
+    printf("========================================\n");
+    printf("      CONFIRMACAO DE DOACAO\n");
+    printf("========================================\n\n");
+    printf("Codigo do doador: %s\n", codigo);
+    printf("Valor doado: R$ %s\n", valor);
+    printf("Metodo de pagamento: %s\n", metodo);
+    printf("\nStatus: DOACAO CONFIRMADA\n");
+
+    return 0;
+}
+
